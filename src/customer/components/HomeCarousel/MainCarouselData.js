eddaa01e86c48a468463d7fd4e0b017d556d055d
LVQT <levietquocthinh@gmail.com>
2024-05-01 17:40:45 +0700

    Added Carousel

---
+++ src/customer/components/HomeCarousel/MainCarouselData.js
@@ -0,0 +1,28 @@
+export const MainCarouselData = [
+
+    {
+        image: "https://external-preview.redd.it/gHJEV2oAcB5aPkNNsLy4H7rD3pGreaQV4aqz11gdQB0.jpg?auto=webp&s=f3131fa4ce18e3bde3cc2217ffa469a6fb26c2eb",
+        path: "/women/clothing/lengha_choli"
+    },
+    {
+        image: "https://external-preview.redd.it/gHJEV2oAcB5aPkNNsLy4H7rD3pGreaQV4aqz11gdQB0.jpg?auto=webp&s=f3131fa4ce18e3bde3cc2217ffa469a6fb26c2eb",
+        path: "/women/clothing/lengha_choli"
+    },
+    {
+        image: "https://external-preview.redd.it/gHJEV2oAcB5aPkNNsLy4H7rD3pGreaQV4aqz11gdQB0.jpg?auto=webp&s=f3131fa4ce18e3bde3cc2217ffa469a6fb26c2eb",
+        path: "/women/clothing/lengha_choli"
+    },
+    {
+        image: "https://external-preview.redd.it/gHJEV2oAcB5aPkNNsLy4H7rD3pGreaQV4aqz11gdQB0.jpg?auto=webp&s=f3131fa4ce18e3bde3cc2217ffa469a6fb26c2eb",
+        path: "/women/clothing/lengha_choli"
+    },
+
+
+
+]
+
+
+
+
+
+
