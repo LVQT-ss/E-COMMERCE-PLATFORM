13a5ab1da288405ccf6ebe9dabb64af78e4a9e31
LVQT <levietquocthinh@gmail.com>
2024-05-05 17:49:46 +0700

    add product detail template

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Footer from './customer/components/Footer/Footer';
 
 import Navigation from './customer/components/Navigation/Navigation';
 import Product from './customer/components/Product/Product';
+import { ProductDetails } from './customer/components/ProductDetails/ProductDetails';
 
 
 import HomePage from './customer/pages/HomePage/HomePage';
@@ -15,7 +16,8 @@ function App() {
 
       <div>
         {/* <HomePage /> */}
-        <Product/>
+        {/* <Product/> */}
+        <ProductDetails/>
 
       </div>
       <Footer/>
