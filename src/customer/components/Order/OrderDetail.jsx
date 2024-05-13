1f586653efc7ddb49fe2ae958d86f3dc9721a96f
LVQT <levietquocthinh@gmail.com>
2024-05-14 02:58:24 +0700

    complete OrderDetail

--- src/customer/components/Order/OrderDetail.jsx
+++ src/customer/components/Order/OrderDetail.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import AdressCard from '../AdressCard/AdressCard'
 import OrderTraker from './OrderTraker'
-import { Grid } from '@mui/material'
+import { Box, Grid } from '@mui/material'
+import { deepPurple } from '@mui/material/colors'
+import StarBorderIcon from '@mui/icons-material/StarBorder';
 
 const OrderDetail = () => {
   return (
@@ -17,20 +19,35 @@ const OrderDetail = () => {
                 <OrderTraker activeStep={3}/>
             </div>
 
-            <Grid className='space-x-5' container>
+            <Grid className='space-y-5' container>
+{[1,1,1,1].map((item) =>
+
                     <Grid className='shadow-xl rounded-md p-5 border' sx={{alignItems:"center",justifyContent:"space-between"}} item container>
                             <Grid item xs={6}>
-                                        <div>
+                                        <div className='flex items-center space-x-4'>
                                             <img className='w-[5rem] h-[5rem] object-cover object-top' src="https://th.bing.com/th/id/OIP.q_rjMwTEEh8ov-IKHOhHVAHaHa?rs=1&pid=ImgDetMain" alt="" />
                                             <div className='space-y-2 ml-5'>
-                                                <p>Mem superslim nigga</p>
-                                                <p className='space-x-5'><span>color:pink</span>Size:M</p>
+                                                <p className='font-semibold'>Mem superslim nigga</p>
+                                                <p className='space-x-5 opacity-50 text-xs font-semibold'><span>color:pink</span>    Size:M</p>
                                                 <p>Seller:linaria</p>
                                                 <p>$1099</p>
                                             </div>
                                         </div>
                             </Grid>
+
+                            <Grid item>
+                                <Box sx={{color:deepPurple[500]}}>
+                                    <StarBorderIcon sx={{fontSize:"2rem" }} className="px-2"/>
+                                    <span>rate & review product</span>
+                                </Box>
+                            </Grid>
+
                     </Grid>
+
+)}
+
+
+
             </Grid>
 
     </div>
